09ca2e43221f8e613f41c3df889d9b7a20af60f1
Unknown <363132274@qq.com>
2018-01-29 12:07:24 +0800

    无闪变效果的Dropdown初始化

--- Assets/Scripts/GuiAfterDrag/OpenDropDown.cs
+++ Assets/Scripts/GuiAfterDrag/OpenDropDown.cs
@@ -4,31 +4,38 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class OpenDropDown : MonoBehaviour {
+public class OpenDropDown : MonoBehaviour, IPointerUpHandler
+{
 
 	// Use this for initialization
 	void Start () {
         //GetComponent<Dropdown>().op
         GetComponent<Dropdown>().onValueChanged.AddListener(ClickDropdown);//Dropdown List
     }
-    void Dropdown1(PointerEventData pointerEventsData)
+
+    bool pointerUp = false;
+    public void OnPointerUp(PointerEventData eventData)
     {
-
-
+        pointerUp = true;
     }
+
+    void Update()
+    {
+        if (pointerUp&&transform.Find("Dropdown List") && transform.Find("Dropdown List").gameObject.GetComponent<RectTransform>().sizeDelta.y!=110)
+        {
+            pointerUp = false;
+            Debug.Log("sda" + transform.Find("Dropdown List").gameObject.GetComponent<RectTransform>().sizeDelta.y);
+            Vector2 size = transform.Find("Dropdown List").gameObject.GetComponent<RectTransform>().sizeDelta;
+            size.y = 110;
+            transform.Find("Dropdown List").gameObject.GetComponent<RectTransform>().sizeDelta = size;
+        }
+    }
+
+
 
     private void ClickDropdown(int index)
     {
         Debug.Log("sda"+ index);
     }
 
-    void Dropdown(int isTrue)
-    {
-
-
-    }
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
